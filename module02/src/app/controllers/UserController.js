1c611a63404dd0d3b9fdd05164274e8d5984d276
Guilherme de Favere <guilherme.favere@gmail.com>
2020-01-29 21:43:54 -0300

    Fix route that update user

--- module02/src/app/controllers/UserController.js
+++ module02/src/app/controllers/UserController.js
@@ -13,7 +13,24 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, old_password } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists)
+        return res.status(400).json({ error: 'User already exists' });
+    }
+
+    if (old_password && !(await user.checkPassword(old_password))) {
+      return res.status(400).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
