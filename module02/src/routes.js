a4612f1fca38731e823e67937971210e2d53a842
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-01 18:33:20 -0300

    Create route that displays the users's appointments

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -19,6 +19,7 @@ routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
