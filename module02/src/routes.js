162fb2eaa851a24c3186c08c754a34a2b7df2feb
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-03 09:36:52 -0300

    Create route that update notification to read

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -25,6 +25,7 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 routes.get('/providers', ProviderController.index);
 routes.get('/schedule', ScheduleController.index);
 routes.put('/users', UserController.update);
