83f158d3b45c37d98a2fd79b944416c1e5c017b3
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-03 13:07:58 -0300

    Create route that cancel appointment

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -23,6 +23,7 @@ routes.use(authMiddleware);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
