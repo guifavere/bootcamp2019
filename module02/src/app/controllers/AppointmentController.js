3174dec689b026db4f33ce21afe1ba2ae671d389
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-03 13:47:06 -0300

    Config email template using handlebars

--- module02/src/app/controllers/AppointmentController.js
+++ module02/src/app/controllers/AppointmentController.js
@@ -94,7 +94,8 @@ class AppointmentController {
   async delete(req, res) {
     const appointment = await Appointment.findByPk(req.params.id, {
       include: [
-        { model: User, as: 'provider', attributes: ['id', 'name', 'email'] },
+        { model: User, as: 'provider', attributes: ['name', 'email'] },
+        { model: User, as: 'user', attributes: ['name'] },
       ],
     });
 
@@ -117,7 +118,14 @@ class AppointmentController {
     await Mail.sendEmail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
