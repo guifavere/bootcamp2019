10eb52198148d2aca98659f86f951030d15e98c0
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-03 08:43:05 -0300

    Create route that displays the appointments for the user provider

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -7,6 +7,7 @@ import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
+import ScheduleController from './app/controllers/ScheduleController';
 import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/midllewares/auth';
@@ -23,6 +24,7 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+routes.get('/schedule', ScheduleController.index);
 routes.put('/users', UserController.update);
 
 export default routes;
