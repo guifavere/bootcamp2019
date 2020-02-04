a2cd7c3a88a620644ff49fad0e786f790a996998
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-04 10:14:12 -0300

    Create the app container component

--- module04/src/index.js
+++ module04/src/index.js
@@ -1,3 +1,9 @@
-const soma = (a, b) => a + b;
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-alert(soma(1, 4));
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
