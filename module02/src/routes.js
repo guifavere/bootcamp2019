23e8a0413f59ffb826e925988a08d8fa387a48a3
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-03 22:35:20 -0300

    Create route that displays availables appointments by user provider

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import AppointmentController from './app/controllers/AppointmentController';
+import AvailableController from './app/controllers/AvailableController';
 import FileController from './app/controllers/FileController';
 import NotificationController from './app/controllers/NotificationController';
 import ProviderController from './app/controllers/ProviderController';
@@ -28,6 +29,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 routes.get('/schedule', ScheduleController.index);
 routes.put('/users', UserController.update);
 
