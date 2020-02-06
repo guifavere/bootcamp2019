63183eb24633e850e179df564b057deda1ac0472
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-06 09:09:17 -0300

    Create style for the repository page

--- module05/src/pages/Repository/index.js
+++ module05/src/pages/Repository/index.js
@@ -1,5 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { Loading, Owner, IssuesList } from './styles';
+
+export default class Repository extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      repository: {},
+      issues: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: { state: 'open', per_page: 5 },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">voltar aos repositórios</Link>
+          <img
+            alt={repository.owner.login}
+            src={repository.owner.avatar_url}
+            title={repository.owner.login}
+          />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+        <IssuesList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img
+                alt={issue.user.login}
+                src={issue.user.avatar_url}
+                title={issue.user.login}
+              />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssuesList>
+      </Container>
+    );
+  }
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
