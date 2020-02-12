eff9c99da2177782eaeb3f8a18aa84b85b031075
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-12 08:24:02 -0300

    Config eslint, and prettier for react native project

---
+++ module06/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, ScrollView, StatusBar } from 'react-native';
+
+import { Header, Colors } from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}
+        >
+          <Header />
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
