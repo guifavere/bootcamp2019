4d09b44b1ef955365aa5be7aefbefa65a5bfbfb7
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-12 09:24:45 -0300

    Config react navigation

--- module06/src/index.js
+++ module06/src/index.js
@@ -1,46 +1,12 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  StatusBar,
-  Text,
-} from 'react-native';
+
 import './config/ReactotronConfig';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-});
+import 'react-native-gesture-handler';
+import Routes from './routes';
 
 const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          <Text>some text</Text>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+  return <Routes />;
 };
 
 export default App;
