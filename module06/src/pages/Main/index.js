cb8c46ff0479a036a386ed3cf0d605d9c97ee343
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-13 08:57:28 -0300

    Get user from github api

--- module06/src/pages/Main/index.js
+++ module06/src/pages/Main/index.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autocorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newUser: '',
+      users: [],
+    };
+  }
+
+  handleAddUser = async () => {
+    const { newUser, users } = this.state;
+
+    const response = await api.get(`users/${newUser}`);
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autocorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
