b3c86d661e568ca2f66588d4731648b6d860b9b4
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-17 08:43:05 -0300

    Add loading during profile searching

--- module06/src/pages/Main/index.js
+++ module06/src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '../../services/api';
@@ -25,11 +25,14 @@ export default class Main extends Component {
     this.state = {
       newUser: '',
       users: [],
+      loading: false,
     };
   }
 
   handleAddUser = async () => {
     const { newUser, users } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`users/${newUser}`);
     const data = {
@@ -42,13 +45,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const { newUser, users } = this.state;
+    const { newUser, users, loading } = this.state;
 
     return (
       <Container>
@@ -62,8 +66,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
         <List
