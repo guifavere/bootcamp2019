5f2dcc8908714f621e9abeb13f573dfb0e95669d
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-17 13:21:20 -0300

    Get the starred repos from selected profile

--- module06/src/pages/User/index.js
+++ module06/src/pages/User/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
 
-export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
+import api from '../../services/api';
 
-  return <View />;
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stars: [],
+    };
+  }
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
 }
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
