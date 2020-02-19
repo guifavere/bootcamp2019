9284b5564a1c0cc8851abca6b154441639929563
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-18 22:07:35 -0300

    Create style for the home page

--- module07/src/pages/Home/index.js
+++ module07/src/pages/Home/index.js
@@ -1,5 +1,97 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
+
+import { ProductsList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductsList>
+      <li>
+        <img
+          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
+          alt="Game legal"
+        />
+        <strong>
+          Game muito legal com nome grande que quebra várias linhas
+        </strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
+          alt="Game legal"
+        />
+        <strong>Game muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
+          alt="Game legal"
+        />
+        <strong>Game muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
+          alt="Game legal"
+        />
+        <strong>Game muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
+          alt="Game legal"
+        />
+        <strong>Game muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
+          alt="Game legal"
+        />
+        <strong>Game muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductsList>
+  );
 }
