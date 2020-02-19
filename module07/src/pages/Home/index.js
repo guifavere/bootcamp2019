1efbd7f1ab2418376af7fdb6dc5955ea3ac835a8
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-18 22:56:10 -0300

    Search products from api

--- module07/src/pages/Home/index.js
+++ module07/src/pages/Home/index.js
@@ -1,97 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
 
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
 import { ProductsList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductsList>
-      <li>
-        <img
-          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
-          alt="Game legal"
-        />
-        <strong>
-          Game muito legal com nome grande que quebra várias linhas
-        </strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
-          alt="Game legal"
-        />
-        <strong>Game muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
-          alt="Game legal"
-        />
-        <strong>Game muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
-          alt="Game legal"
-        />
-        <strong>Game muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
-          alt="Game legal"
-        />
-        <strong>Game muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static3.tcdn.com.br/img/img_prod/187970/beast_quest_ps4_14843_1_20180322230938.jpg"
-          alt="Game legal"
-        />
-        <strong>Game muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductsList>
-  );
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductsList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductsList>
+    );
+  }
 }
