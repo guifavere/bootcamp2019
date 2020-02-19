51029f550a58d5d8fe5bab58734749638f0ef1b9
Guilherme de Favere <guilherme.favere@gmail.com>
2020-02-19 08:53:31 -0300

    Display products at cart page

--- module07/src/store/modules/cart/reducer.js
+++ module07/src/store/modules/cart/reducer.js
@@ -1,9 +1,13 @@
 export default function cart(state = [], action) {
-  console.log(state);
-
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return [
+        ...state,
+        {
+          ...action.product,
+          amount: 1,
+        },
+      ];
     default:
       return state;
   }
